511adf4965457f2e21abe912efb7ce68b13a49ef
Lasse Wenger <lassewenger@gmail.com>
2017-05-11 22:23:06 +0200

    Update on Period_5

---
+++ Period_5/musicians/src/components/musicians.js
@@ -0,0 +1,10 @@
+
+let musicians= [
+      {id: 1, name: "James Hetfield", stars: 8},
+      {id: 2, name: "Tina Turner", stars: 6},
+      {id: 3, name: "Chris Martin", stars: 8},
+      {id: 4, name: "Madonna", stars: 5},
+      {id: 5, name: "Emmelie de Forest", stars: 1}  
+ ]
+
+export default musicians;
